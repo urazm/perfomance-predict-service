6e842ed2a0d4f612051fe6be370f100461f53e17
Gueren Lanelle <guerenla@si-e3.kzn.21-school.ru>
2024-05-11 05:47:19 +0300

    grant predict route add

---
+++ app.py
@@ -0,0 +1,34 @@
+import joblib
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def main_test_func():
+    if request.method == 'POST':
+        return "You sent POST request"
+    else:
+        return "You sent GET request"
+
+
+model = joblib.load('models/itis_grant_model.pkl')
+
+
+@app.route('/grant_predict', methods=['POST'])
+def grant_predict():
+    data = request.json
+    average_score = data['average_score']
+    grant_student_count = data['grant_student_count']
+    grant_student_applied = data['grant_student_applied']
+
+    input_data = [[average_score, grant_student_count, grant_student_applied]]
+
+    prediction = model.predict(input_data)
+
+    response = {'prediction': prediction[0]}
+    return jsonify(response), 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
